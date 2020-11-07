d9b7b1a7b642993a2ef9beaff13eda9f29bdcf2a
Greyun <yhy3587@gmail.com>
2020-11-07 20:49:54 +0900

    1-4 refactor generateNumbers

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,15 +3,26 @@ package game;
 import java.util.Random;
 
 public class Game {
+
+    private final int LENGTH_OF_NUMBER = 3;
+
     public Game() {
     }
 
     public void start() {
-        int[] numbers = generateNumbers();
+        int[] numbers = generateNumbers(LENGTH_OF_NUMBER);
     }
 
-    int[] generateNumbers() {
+    int[] generateNumbers(int numberLength) {
+        int[] numbers = new int[numberLength];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = getRandomNumber();
+        }
+        return numbers;
+    }
+
+    private int getRandomNumber() {
         Random random = new Random();
-        return new int[]{random.nextInt(9) + 1, random.nextInt(9) + 1, random.nextInt(9) + 1};
+        return random.nextInt(9) + 1;
     }
 }
--- src/game/GameTest.java
+++ src/game/GameTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameTest {
+    private final int LENGTH_OF_NUMBER = 3;
+
     @Test
     @DisplayName("generateNumbers 가 적절한 번호를 리턴 해야 한다.")
     void generateNumbersTest() {
@@ -17,14 +19,14 @@ class GameTest {
     @DisplayName("generateNumbers 로 생성된 번호는 3자리 여야 한다.")
     void should_return_3length_intArray_in_generateNumbers() {
         Game game = new Game();
-        int[] numbers = game.generateNumbers();
+        int[] numbers = game.generateNumbers(LENGTH_OF_NUMBER);
         assertEquals(3, numbers.length);
     }
 
     @DisplayName("generateNumbers 로 생성된 번호는 1과 9 사이의 숫자 여야 한다.")
     void should_return_number_between_1_and_9_in_generateNumbers() {
         Game game = new Game();
-        int number = game.generateNumbers()[0];
+        int number = game.generateNumbers(LENGTH_OF_NUMBER)[0];
         assertTrue(number >= 1 && number <= 9);
     }
 }
