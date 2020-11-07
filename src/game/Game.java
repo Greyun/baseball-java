b5ebb868e11ce87c10b0c3b478d630d010b023e7
Greyun <yhy3587@gmail.com>
2020-11-07 20:43:18 +0900

    1-2 번호 생성 로직 test 추가

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
@@ -8,8 +7,12 @@ public class Game {
     }
 
     public void start() {
+        int[] numbers = generateNumbers();
+    }
+
+    int[] generateNumbers() {
         Random random = new Random();
         int[] number = {random.nextInt(9) + 1, random.nextInt(9) + 1, random.nextInt(9) + 1};
-        System.out.println(Arrays.toString(number));
+        return number;
     }
 }
---
+++ src/game/GameTest.java
@@ -0,0 +1,30 @@
+package game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GameTest {
+    @Test
+    @DisplayName("generateNumbers 가 적절한 번호를 리턴 해야 한다.")
+    void generateNumbersTest() {
+        should_return_3length_intArray_in_generateNumbers();
+        should_return_number_between_1_and_9_in_generateNumbers();
+    }
+
+    @DisplayName("generateNumbers 로 생성된 번호는 3자리 여야 한다.")
+    void should_return_3length_intArray_in_generateNumbers() {
+        Game game = new Game();
+        int[] numbers = game.generateNumbers();
+        assertEquals(3, numbers.length);
+    }
+
+    @DisplayName("generateNumbers 로 생성된 번호는 1과 9 사이의 숫자 여야 한다.")
+    void should_return_number_between_1_and_9_in_generateNumbers() {
+        Game game = new Game();
+        int number = game.generateNumbers()[0];
+        assertTrue(number >= 1 && number <= 9);
+    }
+}
