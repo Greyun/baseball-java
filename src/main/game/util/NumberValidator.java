17190cccc2421ed9b7d43018ee3ce6caa819ac77
Greyun <yhy3587@gmail.com>
2020-11-07 22:23:41 +0900

    2-7. validator 로직 수정

--- src/main/game/util/NumberValidator.java
+++ src/main/game/util/NumberValidator.java
@@ -3,10 +3,9 @@ package main.game.util;
 public class NumberValidator {
     static boolean hasNoSameNumberInArray(int[] intArray) {
         for (int i = 0; i < intArray.length; i++) {
-            int j = i + 1;
-            if (j ==  intArray.length) j = 0;
-            if (intArray[i] == intArray[j]) {
-                return false;
+            for (int j = 0; j < intArray.length; j++) {
+                if (i == j) continue;
+                if (intArray[i] == intArray[j]) return false;
             }
         }
         return true;
