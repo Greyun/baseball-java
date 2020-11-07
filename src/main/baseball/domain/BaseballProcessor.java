b9dc568d7e2d2b3efe52c7563218f4604d2d6d68
Greyun <yhy3587@gmail.com>
2020-11-08 00:20:17 +0900

    3-1. method extract getStrike, getBall

--- src/main/baseball/domain/BaseballProcessor.java
+++ src/main/baseball/domain/BaseballProcessor.java
@@ -7,15 +7,17 @@ import java.util.Objects;
 public class BaseballProcessor {
 
     public BaseballResult getProcessResult(int[] a, int[] b) {
-        Strike strike = null;
-        Ball ball = null;
+        Strike strike = getStrike(a, b);
+        Ball ball = getBall(a, b);
 
-        int strikeCount = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == b[i]) strikeCount++;
-        }
-        if (strikeCount > 0) strike = new Strike(strikeCount);
+        if (Objects.nonNull(strike) && Objects.nonNull(ball)) return new StrikeWithBall(strike, ball);
+        if (Objects.nonNull(strike)) return strike;
+        if (Objects.nonNull(ball)) return ball;
 
+        return new Nothing();
+    }
+
+    private Ball getBall(int[] a, int[] b) {
         int ballCount = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b.length; j++) {
@@ -23,12 +25,16 @@ public class BaseballProcessor {
                 if (a[i] == b[j]) ballCount++;
             }
         }
-        if (ballCount > 0) ball = new Ball(ballCount);
+        if (ballCount > 0) return new Ball(ballCount);
+        return null;
+    }
 
-        if (Objects.nonNull(strike) && Objects.nonNull(ball)) return new StrikeWithBall(strike, ball);
-        if (Objects.nonNull(strike)) return strike;
-        if (Objects.nonNull(ball)) return ball;
-
-        return new Nothing();
+    private Strike getStrike(int[] a, int[] b) {
+        int strikeCount = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) strikeCount++;
+        }
+        if (strikeCount > 0) return new Strike(strikeCount);
+        return null;
     }
 }
