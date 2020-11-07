52a683b22f67417314a451cc54edfc1fa96ebc13
Greyun <yhy3587@gmail.com>
2020-11-07 21:32:46 +0900

    2-2. extract method getUserInput, getIntArrayFromString

--- src/main/game/Game.java
+++ src/main/game/Game.java
@@ -16,24 +16,27 @@ public class Game {
         int[] computerNumbers = generateNumbers();
         System.out.println(Arrays.toString(computerNumbers));
 
+        System.out.println("연속된 3개의 숫자를 입력하세요. ex) 123");
         int[] userNumbers = receiveNumbersFromUser();
         System.out.println(Arrays.toString(userNumbers));
     }
 
     private int[] receiveNumbersFromUser() {
-        System.out.println("연속된 3개의 숫자를 입력하세요. ex) 123");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
+        return getIntArrayFromString(getUserInput());
+    }
+
+    private int[] getIntArrayFromString(String userInput) {
         String[] userInputArray = userInput.split("");
         int[] userNumbers = new int[LENGTH_OF_NUMBER];
         for (int i = 0; i < userInputArray.length; i++) {
-            try {
-                userNumbers[i] = Integer.parseInt(userInputArray[i]);
-            } catch (NumberFormatException npe) {
-                npe.printStackTrace();
-            }
+            userNumbers[i] = Integer.parseInt(userInputArray[i]);
         }
         return userNumbers;
+    }
+
+    private String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     private int[] generateNumbers() {
