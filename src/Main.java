bc26354120e26332f28b1287034f0b600c34cd84
Greyun <yhy3587@gmail.com>
2020-11-07 21:16:24 +0900

    2. 플레이어 숫자 입력 받는 로직 추가

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import game.Game;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
--- src/game/Game.java
+++ src/game/Game.java
@@ -2,6 +2,9 @@ package game;
 
 import game.util.NumberGenerator;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Game {
 
     private final int LENGTH_OF_NUMBER = 3;
@@ -11,6 +14,21 @@ public class Game {
 
     public void start() {
         int[] computerNumbers = generateNumbers();
+        System.out.println(Arrays.toString(computerNumbers));
+
+        System.out.println("연속된 3개의 숫자를 입력하세요. ex) 123");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] userInputArray = userInput.split("");
+        int[] userNumber = new int[LENGTH_OF_NUMBER];
+        for (int i = 0; i < userInputArray.length; i++) {
+            try {
+                userNumber[i] = Integer.parseInt(userInputArray[i]);
+            } catch (NumberFormatException npe) {
+                npe.printStackTrace();
+            }
+        }
+        System.out.println(Arrays.toString(userNumber));
     }
 
     private int[] generateNumbers() {
